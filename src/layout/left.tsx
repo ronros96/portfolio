d96171d6af7aeca4b786383a18dd16ffd9964bc9
Aaron Rosales <p.aaronrosales@gmail.com>
2026-05-12 15:20:18 +0800

    update left side layout with mobile view

--- src/layout/left.tsx
+++ src/layout/left.tsx
@@ -10,12 +10,12 @@ const Left = () =>{
   return(
     <div className="container">
       <div className='content'>
-        <h1 className='header-3'>
+        <h1>
           <span className='text-[var(--pri-purple)]'>Hello I'm</span><br/>AARON ROSALES
         </h1>
         <div>
           <p>Hi, I'm Aaron, a seasoned Web Developer and Creative Artist. I have a deep understanding of modern front-end technologies and a keen eye for design. Over {year()} years, I've worked with a variety of clients, creating visually appealing and user-friendly interfaces for their websites and applications.</p>
-          <p><a href="./resume.pdf" target='_blank'><span>See my Resume</span></a></p>
+          <p><a href="./resume.pdf" target='_blank'><span>View my Resume</span></a></p>
         </div>
       </div>
     </div>
